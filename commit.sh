989be64d84da601c9dd0c6624b2b727b908ea521
Ali <a.ahmad9427@gmail.com>
2022-03-23 16:55:22 -0400

    Regular auto-commit at 16:55:22 on 23/03/2022

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cp ../1.world.gzip /.
+cp "/c/Program Files (x86)/Steam/userdata/83744553/1621690/remote/worlds/1.world.gzip" .
 git add -f ./1.world.gzip
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
 git commit -am "Regular auto-commit $(timestamp)"
-git push origin master
+git push
